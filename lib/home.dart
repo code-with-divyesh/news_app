3eb28af86871fd148fe757543aa3b2652f49f75c
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2024-07-31 10:40:39 +0530

    slider created

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -93,10 +92,38 @@ class _HomeState extends State<Home> {
                       ),
                     );
                   }),
-            )
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: 230,
+                  autoPlay: true,
+                  enableInfiniteScroll: false,
+                  enlargeCenterPage: true),
+              items: items.map((item) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return InkWell(
+                      onTap: () {
+                        print(item);
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 5.0, vertical: 14.0),
+                        decoration: BoxDecoration(
+                          color: item,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
           ],
         ),
       ),
     );
   }
+
+  final List items = [Colors.black, Colors.orange, Colors.blue, Colors.yellow];
 }
